c4762f53966f54a81acc41924fc7e3019420d5b4
姚超臣 <yaochaochen@ctv.org.cn>
2019-04-14 15:56:22 +0800

    自定义拦截器

--- merchants/src/main/java/com/passbook/merchants/MerchantsApplication.java
+++ merchants/src/main/java/com/passbook/merchants/MerchantsApplication.java
@@ -1,13 +1,31 @@
 package com.passbook.merchants;
 
+import com.passbook.merchants.security.AuthCheckInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
-public class MerchantsApplication {
+public class MerchantsApplication extends WebMvcConfigurerAdapter {
 
+    
+    @Resource
+    private AuthCheckInterceptor authCheckInterceptor;
+    
     public static void main(String[] args) {
         SpringApplication.run(MerchantsApplication.class, args);
     }
 
+    /**
+     * 重载拦截器 拦截自定义Contr
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authCheckInterceptor).addPathPatterns("/merchants/**");
+        super.addInterceptors(registry);
+    }
 }
